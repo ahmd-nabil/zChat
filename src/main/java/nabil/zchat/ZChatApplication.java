82aba434b6a33b1c48c834b4abe457536431e159
ahmd-nabil <ahm3dnabil99@gmail.com>
2023-10-28 12:59:09 +0200

    project created

---
+++ src/main/java/nabil/zchat/ZChatApplication.java
@@ -0,0 +1,13 @@
+package nabil.zchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZChatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nabil/zchat/ZChatApplicationTests.java
@@ -0,0 +1,13 @@
+package nabil.zchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZChatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
