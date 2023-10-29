a978750ca476a4753b58e0c4f781ff2d814ca359
ahmd-nabil <ahm3dnabil99@gmail.com>
2023-10-29 17:03:50 +0200

    added security dependency and basic configs

---
+++ src/main/java/nabil/zchat/configs/SecurityConfig.java
@@ -0,0 +1,42 @@
+package nabil.zchat.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+/**
+ * @author Ahmed Nabil
+ */
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .cors(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(
+                        configurer -> configurer
+                                .requestMatchers("/ws/**", "/getAuth/**").permitAll()
+                                .anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults())
+                .formLogin(Customizer.withDefaults())
+                .build();
+    }
+
+    @Bean
+    public UserDetailsService inMemoryUsers() {
+        UserDetails user1 = User.builder().username("ahmed").password("{noop}password").build();
+        UserDetails user2 = User.builder().username("nabil").password("{noop}password").build();
+        return new InMemoryUserDetailsManager(user1, user2);
+    }
+}
