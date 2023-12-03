77017b0b8f204c65b8b03f7887ef6a9aa2632c10
ahmd-nabil <ahm3dnabil99@gmail.com>
2023-12-03 22:36:36 +0200

    add newChat using userSubjects

--- src/main/java/nabil/zchat/controllers/ChatController.java
+++ src/main/java/nabil/zchat/controllers/ChatController.java
@@ -8,9 +8,12 @@ import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -33,5 +36,12 @@ public class ChatController {
     @GetMapping
     public List<ChatResponse> getAllUserChats(Authentication authentication) {
         return this.chatService.getAllChatsByUserSubject(authentication.getName());
+    }
+
+    @ResponseBody
+    @PostMapping()
+    public ResponseEntity<Void> addNewChat(@RequestBody ArrayList<String> chatUsersSubjects) {
+        chatService.addNewChat(chatUsersSubjects);
+        return ResponseEntity.ok().build();
     }
 }
--- src/main/java/nabil/zchat/services/ChatService.java
+++ src/main/java/nabil/zchat/services/ChatService.java
@@ -1,9 +1,12 @@
 package nabil.zchat.services;
 
 import lombok.RequiredArgsConstructor;
+import nabil.zchat.domain.Chat;
+import nabil.zchat.domain.ChatUser;
 import nabil.zchat.dtos.ChatResponse;
 import nabil.zchat.mappers.ChatMapper;
 import nabil.zchat.repositories.ChatRepo;
+import nabil.zchat.repositories.ChatUserRepo;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -16,6 +19,7 @@ import java.util.List;
 public class ChatService {
     private final ChatRepo chatRepo;
     private final ChatMapper chatMapper;
+    private final ChatUserRepo chatUserRepo;
 
     public ChatResponse getChatById(Long id) {
         return this.chatMapper.toChatResponse(this.chatRepo.findById(id).orElseThrow());
@@ -23,5 +27,12 @@ public class ChatService {
 
     public List<ChatResponse> getAllChatsByUserSubject(String subject) {
         return this.chatRepo.getAllChatsByUserSubject(subject).stream().map(this.chatMapper::toChatResponse).toList();
+    }
+
+    public void addNewChat(List<String> chatUsersSubjects) {
+        List<ChatUser> chatUsers = chatUserRepo.findAllBySubject(chatUsersSubjects);
+        if(chatUsers.isEmpty()) throw new RuntimeException("1 user required at least.");
+        Chat newChat = Chat.builder().chatUsers(chatUsers).build();
+        chatRepo.save(newChat);
     }
 }
