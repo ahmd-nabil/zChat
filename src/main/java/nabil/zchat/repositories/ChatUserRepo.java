5975f569c8517fbc0fd1f51f25ed4d4d818c026c
ahmd-nabil <ahm3dnabil99@gmail.com>
2023-12-03 21:42:15 +0200

    add search user by subject and email in repo

--- src/main/java/nabil/zchat/repositories/ChatUserRepo.java
+++ src/main/java/nabil/zchat/repositories/ChatUserRepo.java
@@ -2,7 +2,9 @@ package nabil.zchat.repositories;
 
 import nabil.zchat.domain.ChatUser;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 /**
@@ -10,4 +12,9 @@ import java.util.Optional;
  */
 public interface ChatUserRepo extends JpaRepository<ChatUser, Long> {
     Optional<ChatUser> findBySubject(String subject);
+
+    @Query("select user from ChatUser user where user.subject in :subjects")
+    List<ChatUser> findAllBySubject(List<String> subjects);
+
+    Optional<ChatUser> findByEmail(String email);
 }
