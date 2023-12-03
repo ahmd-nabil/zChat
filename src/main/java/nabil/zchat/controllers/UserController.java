923181385032f58d8f4193579a545ae171f41b16
ahmd-nabil <ahm3dnabil99@gmail.com>
2023-12-03 22:36:11 +0200

    add findUserByEmail endpoint

--- src/main/java/nabil/zchat/controllers/UserController.java
+++ src/main/java/nabil/zchat/controllers/UserController.java
@@ -1,15 +1,25 @@
 package nabil.zchat.controllers;
 
+import lombok.RequiredArgsConstructor;
+import nabil.zchat.domain.ChatUser;
+import nabil.zchat.exceptions.UserNotFoundException;
+import nabil.zchat.repositories.ChatUserRepo;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author Ahmed Nabil
  */
 @RestController
+@RequestMapping()
+@RequiredArgsConstructor
 public class UserController {
+
+    private final ChatUserRepo chatUserRepo;
 
     @GetMapping("/auth")
     private ResponseEntity<Authentication> getAuthentication(Authentication authentication) {
@@ -20,5 +30,11 @@ public class UserController {
     private ResponseEntity<Authentication> getPrivate(Authentication authentication) {
         System.out.println(authentication);
         return ResponseEntity.ofNullable(authentication);
+    }
+
+    @GetMapping("/users")
+    public ResponseEntity<ChatUser> findUserByEmail(@RequestParam(value = "email") String email) {
+        ChatUser chatUser = this.chatUserRepo.findByEmail(email).orElseThrow(UserNotFoundException::new);
+        return ResponseEntity.ok().body(chatUser);
     }
 }
